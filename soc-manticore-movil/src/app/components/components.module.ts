7ae41809a267690c5e8b929bebf65c3009f2e47f
andersonrev <fabiander_21@hotmail.es>
2021-09-17 17:33:06 -0500

    feat: cambio de nombres tabs

--- soc-manticore-movil/src/app/components/components.module.ts
+++ soc-manticore-movil/src/app/components/components.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MapaComponent} from './mapa/mapa.component';
+import {HeaderComponent} from './header/header.component';
+import {IonicModule} from '@ionic/angular';
 
 
 
 @NgModule({
-  declarations: [MapaComponent],
+  declarations: [MapaComponent,HeaderComponent],
   imports: [
     CommonModule,
+    IonicModule,
   ],
-  exports: [MapaComponent]
+  exports: [MapaComponent, HeaderComponent]
 })
 export class ComponentsModule { }
--- soc-manticore-movil/src/app/pages/menu-principal/menu-principal.page.ts
+++ soc-manticore-movil/src/app/pages/menu-principal/menu-principal.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-menu-principal',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuPrincipalPage implements OnInit {
 
-  constructor() { }
+  constructor(private navController: NavController) { }
 
   ngOnInit() {
   }
 
+  irVisita() {
+    this.navController.navigateRoot('main/tabs/ventas')
+
+  }
 }
