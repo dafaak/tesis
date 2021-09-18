1a6e69c02fd7361fc731966eecc38f5d36083e0e
andersonrev <fabiander_21@hotmail.es>
2021-09-18 17:09:07 -0500

    feat: usar leaflet mapas libres no como  mapbox :v

--- soc-manticore-movil/src/app/components/mapa/mapa.component.ts
+++ soc-manticore-movil/src/app/components/mapa/mapa.component.ts
@@ -1,6 +1,6 @@
 import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
+import * as L from 'leaflet';
 
-declare var mapboxgl: any;
 
 @Component({
   selector: 'app-mapa',
@@ -9,14 +9,25 @@ declare var mapboxgl: any;
 })
 export class MapaComponent implements OnInit, AfterViewInit {
 
-  @ViewChild('mapa') mapa;
+  @ViewChild('mapa') map;
+  // map;
 
 
   @Input()
   longitudVendedor: number;
 
   @Input()
-  latitudVendedor: number
+  latitudVendedor: number;
+
+  icon = L.icon(
+    {
+      iconUrl: '/assets/icon/marker.svg',
+      iconSize: [38,95],
+      iconAnchor: [22,94],
+
+    }
+  );
+
 
   constructor() {
   }
@@ -27,68 +38,32 @@ export class MapaComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
 
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYW5kZXJzb25yZXYiLCJhIjoiY2tzMjVrczdvMGVxdzJucGsycHpjNGYybSJ9.69SGPABVoQ3v0ZGc30FDlQ';
-    const map = new mapboxgl.Map({
-      container: this.mapa.nativeElement,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [this.longitudVendedor, this.latitudVendedor],
+    this.initMap();
+
+
+  }
+
+  initMap(): void {
+    this.map = L.map('map', {
+      center: [this.latitudVendedor, this.longitudVendedor],
       zoom: 15,
-      antialias: true
+      renderer: L.canvas()
     });
 
-    map.on('load', () => {
-      map.resize();
+    L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.map);
 
-      const marker1 = new mapboxgl.Marker()
-        .setLngLat([this.longitudVendedor, this.latitudVendedor])
-        .addTo(map);
+    L.marker([this.latitudVendedor, this.longitudVendedor],{
+      icon: this.icon
+    }).addTo(this.map)
+      .bindPopup('Aqui estas.')
+      .openPopup();
 
+    setTimeout(()=> {
+      this.map.invalidateSize()
+    }, 0)
 
-      // Insert the layer beneath any symbol layer.
-      const layers = map.getStyle().layers;
-      const labelLayerId = layers.find(
-        (layer) => layer.type === 'symbol' && layer.layout['text-field']
-      ).id;
-
-
-      map.addLayer(
-        {
-          'id': 'add-3d-buildings',
-          'source': 'composite',
-          'source-layer': 'building',
-          'filter': ['==', 'extrude', 'true'],
-          'type': 'fill-extrusion',
-          'minzoom': 15,
-          'paint': {
-            'fill-extrusion-color': '#aaa',
-
-            // Use an 'interpolate' expression to
-            // add a smooth transition effect to
-            // the buildings as the user zooms in.
-            'fill-extrusion-height': [
-              'interpolate',
-              ['linear'],
-              ['zoom'],
-              15,
-              0,
-              15.05,
-              ['get', 'height']
-            ],
-            'fill-extrusion-base': [
-              'interpolate',
-              ['linear'],
-              ['zoom'],
-              15,
-              0,
-              15.05,
-              ['get', 'min_height']
-            ],
-            'fill-extrusion-opacity': 0.6
-          }
-        },
-        labelLayerId
-      );
-    })
   }
 
 
